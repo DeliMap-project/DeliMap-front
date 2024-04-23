a0db28deeea00b3a93235d6d297e55fc6eac634e
tomhoon <gnsdl9108@naver.com>
2024-04-23 23:00:59 +0900

    test: 테스트 코드 삽입

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios';
+
+
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const test = async () => {
+    let res = await axios.get('https://api.kbland.kr/land-property/property/getPsaleExpsStatcList');
+    console.log(res.data.dataBody.data)
+  }  
+
+  
+  test();
+
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>Hello DeliMap!</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
