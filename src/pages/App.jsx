82ec7f9f4bfebcefd4ce250312c585631640f223
tomhoon <gnsdl9108@naver.com>
2024-04-25 00:53:53 +0900

    chore: router 추가 및 컴포넌트 구조 변경

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,30 +1,24 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import '../styles/App.css'
-// eslint-disable-next-line no-unused-vars
-import Menu from '/src/components/menu/Menu.jsx';
 import axios from 'axios';
-
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import RouteTest1 from './RouteTest1';
+import RouteTest2 from './RouteTest2'
+import DefaultLayer from './DefaultLayer'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   const test = async () => {
     let res = await axios.get('https://api.kbland.kr/land-property/property/getPsaleExpsStatcList');
     console.log(res.data.dataBody.data)
   }
 
+  const router = createBrowserRouter([
+    {path: '/', element: <DefaultLayer/>},
+    {path: '/route1', element: <RouteTest1/>},
+    {path: '/route2', element: <RouteTest2/>},
+  ])
   
-  test();
-
   return (
     <>
-      <div>
-        <p>Hello DeliMap!</p>
-        <Menu/>
-      </div>
+      <RouterProvider router={router}/>
     </>
   )
 }
