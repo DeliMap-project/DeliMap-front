8bd38c18b4bc231bdbc8f82d303c45bf73699327
tomhoon <gnsdl9108@naver.com>
2024-04-25 22:56:19 +0900

    feat: 지도추가

--- src/pages/DefaultLayer.jsx
+++ src/pages/DefaultLayer.jsx
@@ -1,9 +1,21 @@
 import Menu from '/src/components/menu/Menu.jsx';
 import '../styles/default.css'
+import { useEffect } from 'react';
+
+const { kakao } = window;
 
 function DefaultLayer() {
+    useEffect(() => {
+        const container = document.getElementById('map');
+        const options = {
+            center: new kakao.maps.LatLng(33.450701, 126.570667),
+            level: 3
+        }
+    
+        const map = new kakao.maps.Map(container, options);
+      }, []);
     return (
-        <div>
+        <div id="map" style={{width: '1920px', height: '1080px'}}v>
             <Menu/>
         </div>
     )
