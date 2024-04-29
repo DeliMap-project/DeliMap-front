b467f1162048b670cc6c62a9c12ebef143f53deb
남병현 <kmi5063@naver.com>
2024-04-29 19:26:18 +0900

    feat : 게시판 리스트, 게시판 상세보기 모달 수정 중

--- src/components/board/BoardDetail.jsx
+++ src/components/board/BoardDetail.jsx
@@ -3,13 +3,48 @@ import {useState, useEffect} from "react";
 import axios from "axios";
 
 const BoardDetail = () => {
-    const [board_list, setBoardList] = useState([]);
+    const [isDetail, setIsDetail ] = useState([]);
+    const [isDisplay, setIsDisplay ] = useState(false);
+
+    const close = () => {
+        setIsDisplay(!isDisplay);
+    }
+    useEffect( () => {
+        const board_no = localStorage.getItem('board_no');
+        let param = {
+            board_no: board_no,
+        }
+         axios.post('http://localhost:3300/board/detail', param).then(response => {
+            setIsDetail(response.data);
+            const detailBoard = response.data
+            detailBoard.board_no = detailBoard[0].board_no;
+            detailBoard.board_name = detailBoard[0].board_name;
+            detailBoard.board_content = detailBoard[0].board_content;
+            detailBoard.board_phone = detailBoard[0].board_phone;
+            detailBoard.board_address = detailBoard[0].board_address;
+            detailBoard.board_readcnt = detailBoard[0].board_readcnt;
+            detailBoard.review_score = detailBoard[0].review_score;
+         });
+    }, []);
 
     return (
         <>
-            <div className={'detail-container'}>
+            <div className={'detail-container ' + (isDisplay ? 'display-none' : 'display-block')}>
                 <div className={'detail-wrapper'}>
-                    dddddddfsdfsdf
+                    <div className={'modal-close'} onClick={close}><span>X</span></div>
+                    <div>
+                        {
+                            <>
+                                <div>board_no : {isDetail.board_no}</div>
+                                <div>board_name : {isDetail.board_name}</div>
+                                <div>board_content : {isDetail.board_content}</div>
+                                <div>board_phone : {isDetail.board_phone}</div>
+                                <div>board_address : {isDetail.board_address}</div>
+                                <div>board_readcnt : {isDetail.board_readcnt}</div>
+                                <div>review_score : {isDetail.review_score}</div>
+                            </>
+                        }
+                    </div>
                 </div>
             </div>
 
@@ -17,4 +52,6 @@ const BoardDetail = () => {
     )
 
 }
+
+
 export default BoardDetail
--- src/components/board/BoardList.jsx
+++ src/components/board/BoardList.jsx
@@ -1,67 +1,69 @@
 import BoardDetail from "./BoardDetail.jsx";
-import '/src/styles/board/BoardList.css'    ;
+import '/src/styles/board/BoardList.css';
+// import '/src/styles/board/BoardDetail.css';
 import {useState, useEffect } from "react";
 import axios from "axios";
 const BoardList = () => {
-    const [isActive, setIsActive, isDetail, setIsDetail ] = useState(false);
-    const [ isDetail1 ] = useState(true);
+    const [isDetail, setIsDetail ] = useState(false);
+    const [isActive, setIsActive ] = useState(false);
     const [board_list, setBoardList] = useState([]);
 
-    const handleClick = () => {
+    const boardToggle = () => {
         setIsActive(!isActive);
-    };
-    const detailClick = () =>{
-        setIsDetail(!isDetail);
 
     };
+    const boardDetail = (board_no) => {
+        setIsDetail((isDetail) => !isDetail);
+
+        localStorage.setItem('board_no', board_no);
+        console.log('isDetail1 : ', isDetail);
+
+    };
+
     useEffect(() => {
         axios.post('http://localhost:3300/board/list')
             .then(response => {
                 setBoardList(response.data);
+                console.log('isDetail2 : ', isDetail);
             });
     }, []);
         return (
             <>
-                    <div className={'modal-mask ' +  (isActive ? 'animated-button' : 'normal-button')}>
-                        <div className="modal-wrapper">
-                            <div className="modal-x drag-disable">
-                                <span onClick={handleClick}>
-                                    {isActive ? <img className='toggle_open' src={'src/assets/icons8-back-64.png'} alt={'열기'}/> :
-                                        <img className='toggle_close' src={'src/assets/icons8-forward-64.png'} alt={'닫기'}/>}
-                                </span>
-                            </div>
-                            <div className="modal-container">
-                                <div className="modal-header">
-                                    <div className="board_local">영등포구 여의도동</div>
-                                    <slot name="header">
-                                        <div className="board_list">
-                                            {board_list.map(boardList => {
-                                                return (
-                                                    <div key={boardList.board_no}>
-                                                        <div className="board_list_name" onClick={detailClick}>{boardList.board_name}</div>
-                                                        <div className="board_list_content">{boardList.board_content}</div>
-                                                        <div className="board_list_phone">{boardList.board_phone}</div>
-                                                        <div className="board_list_review">리뷰 : {boardList.review_cnt}</div>
-                                                        <div className="board_list_hashtag">{boardList.review_hashtag}</div>
-                                                        <div className="board_list_img"><img src="/src/assets/고양이1.jpg" alt="이미지"/></div>
-                                                        <hr className="list_hr"/>
-                                                    </div>
-                                                )
-                                            })}
-                                        </div>
-                                    </slot>
-                                </div>
+                <div className={'modal-mask ' + (isActive ? 'animated-button' : 'normal-button')}>
+                    <div className="modal-wrapper">
+                        <div className="modal-x drag-disable">
+                            <span onClick={boardToggle}>
+                                {isActive ? <img className='toggle_open' src={'src/assets/icons8-back-64.png'} alt={'열기'}/> :
+                                    <img className='toggle_close' src={'src/assets/icons8-forward-64.png'} alt={'닫기'}/>}
+                            </span>
+                        </div>
+                        <div className="modal-container">
+                            <div className="modal-header">
+                                <div className="board_local">영등포구 여의도동</div>
+                                <slot name="header">
+                                    <div className="board_list">
+                                        {board_list.map(boardList => {
+                                            return (
+                                                <div key={boardList.board_no}>
+                                                    <div className="board_list_name"><span onClick={() => boardDetail(boardList.board_no)}>{boardList.board_name}</span></div>
+                                                    <div className="board_list_content">{boardList.board_content}</div>
+                                                    <div className="board_list_phone">{boardList.board_phone}</div>
+                                                    <div className="board_list_review">리뷰 : {boardList.review_cnt}</div>
+                                                    <div className="board_list_hashtag">{boardList.review_hashtag}</div>
+                                                    <div className="board_list_img"><img src="/src/assets/고양이1.jpg" alt="이미지"/></div>
+                                                    <hr className="list_hr"/>
+                                                </div>
+                                            )
+                                        })}
+                                    </div>
+                                </slot>
                             </div>
                         </div>
                     </div>
+                </div>
                 {isDetail ? <BoardDetail /> : null}
             </>
         )
 }
 
-// const aa = async () => {
-//     const res = await axios.post('http://localhost:3300/board/list');
-//     this.board_list = res.data;
-//     console.log('board_list : ', this.board_list)
-// }
 export default BoardList
